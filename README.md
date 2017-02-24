1d7c7339e059d7c2bef41b58468bee7d8549eff4
elias <eliasdefrancisco@gmail.com>
2017-02-24 12:04:46 +0100

    Gulp setted for detecting changes on src, auto build, ...

--- README.md
+++ README.md
@@ -17,13 +17,26 @@ Browser auto reloader
 https://browsersync.io/
 
 ### Browserify
-Group all requires() on one boundle
+Group all requires() and build up all it's dependencies on one boundle
+You don't have to add <script> tags on your html (Only one for the bundle)
 http://browserify.org/
 
 ### Babelify
-Transpile ES6 to ES5 within Browserify
+Transpile ES6 to ES5 within Browserify.transform()
 https://github.com/babel/babelify
 
 ### Gulp-sass
 Generate CSS from SCSS within Gulp
 https://github.com/dlmanning/gulp-sass
+
+### Run-sequence
+Run a sequence of gulp tasks in specefied order
+https://github.com/OverZealous/run-sequence
+
+### Del
+Delete files and return promises
+https://github.com/sindresorhus/del
+
+### Vinyl-source-stream
+Use text streams on gulp
+https://github.com/hughsk/vinyl-source-stream
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require('gulp')
+const BrowserSync = require('browser-sync') // .create() !!!
+const browserify = require('browserify')
+const babelify = require('babelify')
+const sass = require('gulp-sass')
+const runSequence = require('run-sequence')
+const del = require('del')
+const source = require('vinyl-source-stream')
+
+
+gulp.task('browserSync', function(){
+  const browserSync = BrowserSync.create()
+  browserSync.init({
+    server: 'www',
+    notify: false
+  })
+  browserSync.reload()
+})
+
+gulp.task('copyFiles', function(){
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('www'))
+})
+
+gulp.task('styles', function(){
+  return gulp.src('src/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('www'))
+})
+
+gulp.task('browserify', function() {
+  return browserify({
+      entries: 'src/main.js',
+      debug: true })
+    .transform(babelify) //.on('update', updateBrowserify)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('www'))
+})
+
+gulp.task('clean', function() {
+    return del(['www/**', '!www', '!www/font-awesome.ttf'])
+})
+
+gulp.task('build', function(callback) {
+    return runSequence('clean', ['browserify', 'styles', 'copyFiles'], callback)
+})
+
+gulp.task('run', function(callback){
+  return runSequence('build', 'browserSync', callback)
+})
+
+gulp.task('develop', function(callback){
+  // Infinite loop for watching files ...
+  gulp.watch([
+      'src/index.html',
+      'src/**/*.js',
+      'src/assets/**/*',
+      'src/**/*.scss'
+    ],
+    ['develop']
+  )
+  return runSequence('run', callback)
+})
+
+// Default task when gulp is executed by the system
+gulp.task('default', ['develop'])
+
+
+
+
+///
